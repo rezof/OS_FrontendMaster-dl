46739b39f4777842cddcdba573e4bad96cdb0267
boujnane monssef <m-heroine@hotmail.com>
2017-10-13 20:11:13 +0100

    updated to match FEM site updates

--- extractor/spider.py
+++ extractor/spider.py
@@ -16,6 +16,7 @@ DATA_COURSE_LIST              = './DATA_COURSE_LIST.json'
 DATA_COURSE_DETAILED_LIST_CDN = './DATA_COURSE_DETAILED_LIST_CDN.json'
 URL_LOG_IN                    = 'https://frontendmasters.com/login/'
 URL_COURSE_LIST               = 'https://frontendmasters.com/courses/'
+BASE_URL                      = 'https://frontendmasters.com'
 
 class Spider(object):
     def __init__(self):
@@ -25,8 +26,8 @@ class Spider(object):
         self.browser.get(URL_LOG_IN)
         time.sleep(2)
 
-        username_field = self.browser.find_element_by_id('rcp_user_login')
-        password_field = self.browser.find_element_by_id('rcp_user_pass')
+        username_field = self.browser.find_element_by_id('username')
+        password_field = self.browser.find_element_by_id('password')
 
         username_field.send_keys(id)
         password_field.send_keys(password)
@@ -34,8 +35,7 @@ class Spider(object):
 
     def download(self, course):
         # Get detailed course list
-        course_detailed_list = self._get_detailed_course_list(course)
-
+        course_detailed_list = self._get_new_detailed_course_list(course)
         # Get downloadable CDN
         course_downloadbale = self._get_downloadable_links(course_detailed_list)
 
@@ -45,9 +45,8 @@ class Spider(object):
         self.download_course(course_downloadbale)
 
         # self.browser.close()
-
-
-    def _get_detailed_course_list(self, course):
+        
+    def _get_new_detailed_course_list(self, course):
         course_link = URL_COURSE_LIST + course + '/'
         course_detial = {
             'title': course,
@@ -60,61 +59,41 @@ class Spider(object):
         soup_page = BeautifulSoup(self.browser.page_source, 'html.parser')
 
         # Find video nav list
-        sections = soup_page.find('ul', {'class': 'video-nav-list'})
-        sections_items = sections.find_all(
-            'li', {'class': 'video-nav-section'}
-        )
-
-        sections = self._get_section_data(sections_items)
+        all_items = soup_page.find('section', {'class': 'CourseToc'}).find('div', {'class': 's-wrap'}).select(' > *')
+        sections = self._get_new_section_data(all_items)
         course_detial['sections'].extend(sections)
 
         return course_detial
-
-    def _get_section_data(self, sections_items):
+        
+        
+    def _get_new_section_data(self, sections_items):
         sections = []
         for item in sections_items:
             # Course section data structure
-            course_section = {
-                'title': None,
-                'subsections': []
-            }
+            if item.name == 'h3':
+                course_section = {
+                    'title': '',
+                    'subsections': []
+                }
+                course_section['title'] = item.getText()
+                sections.append(course_section)
+            elif item.name == 'ul':
+                elements = item.find_all('a')
+                sections_items = []
+                for el in elements:
+                    vid_link = el.get('href')
+                    title = el.find('div', {'class': 'heading'}).find('h3').getText()
+                    sections_items.append({
+                        'title': title,
+                        'url': vid_link,
+                        'downloadable_url': None
+                    })
+                last_section_index = len(sections) - 1
+                if last_section_index > -1:
+                    sections[last_section_index]['subsections'] = sections_items
 
-            course_section['title'] = item.find(
-                'h4', {'class': 'video-nav-section-title'}
-            ).getText()
-
-            videos_section = item.find('ul')
-            videos_section_items = videos_section.find_all('li')
-
-            videos_data = self._get_videos_data(videos_section_items)
-            course_section['subsections'].extend(videos_data)
-
-            sections.append(course_section)
-
+        sections.append(course_section)
         return sections
-
-    def _get_videos_data(self, videos_section_items):
-        subsections = []
-
-        for video in videos_section_items:
-            # Course subsection data structure
-            course_subsection = {
-                'title': None,
-                'url': None,
-                'downloadable_url': None
-            }
-
-            course_subsection['url'] = video.find('a')['href']
-            title = video.find('a').find(
-                'span', {'class', 'text'}
-            ).find(
-                'span', {'class', 'title'}
-            ).getText()
-
-            course_subsection['title'] = format_filename(title)
-            subsections.append(course_subsection)
-
-        return subsections
 
     def _get_downloadable_links(self, course):
         # course data structure
@@ -129,13 +108,12 @@ class Spider(object):
         for section in course['sections']:
             for subsection in section['subsections']:
                 if subsection['downloadable_url'] is None:
-
                     print("Retriving: {0}/{1}/{2}".format(
                         format_filename(course['title']),
                         format_filename(section['title']),
                         format_filename(subsection['title'])))
 
-                    video_url = url + subsection['url']
+                    video_url = BASE_URL + subsection['url']
                     self.browser.get(video_url)
                     time.sleep(8)
 
@@ -164,16 +142,18 @@ class Spider(object):
 
         for i1, section in enumerate(course['sections']):
             section_title = section['title']
-
+            
+            # Create section directory
+            section_path = './Download/{0}/{1} - {2}'.format(title, i1, section['title'])
+            create_path(section_path)
+            
             for i2, subsection in enumerate(section['subsections']):
                 subsection_title = subsection['title']
                 print("Downloading: {0}".format(
                     format_filename(subsection_title)))
 
-                filename = str(i1) + '-' + str(i2) + format_filename(
-                    section_title) + '|' + format_filename(
-                        subsection_title) + '.mp4'
+                filename = str(i1) + '-' + str(i2) + ' ' + format_filename(subsection_title) + '.mp4'
 
-                file_path = course_path + '/' + format_filename(filename)
+                file_path = section_path + '/' + format_filename(filename)
 
                 download_file(subsection['downloadable_url'], file_path, self)
